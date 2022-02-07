cdfd4c1d7dfff6fe1dc820441aaccc81cc3ab34f
Orlando Marques <realorlanditomadrid@gmail.com>
2022-02-07 14:39:18 +0100

    ADD: AdministratorRepository

---
+++ ComeBien/DataAccess/Repositories/AdministratorRepository.cs
@@ -0,0 +1,29 @@
+﻿using ComeBien.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComeBien.DataAccess.Repositories
+{
+    public class AdministratorRepository : BaseRepository<Administrator>, IAdministratorRepository
+    {
+        public AdministratorRepository()
+        {
+
+        }
+
+        public bool IsAdmin(string userName, string pass)
+        {
+            using (var dbContext = new ComeBienContext())
+            {
+                return dbContext.Administrators.Any(x => x.UserName == userName && x.Password == pass);
+            }
+        }
+    }
+
+    internal interface IAdministratorRepository : IBaseRepository<Administrator>
+    {
+    }
+}
