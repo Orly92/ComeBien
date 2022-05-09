a851d251beab09243717637bacdef4b372e82de2
Orlando Marques <realorlanditomadrid@gmail.com>
2022-05-09 18:53:44 +0200

    ADD: SalesProductVMTest

---
+++ UnitTest.ComeBien/Tests/SalesProductVMTest.cs
@@ -0,0 +1,67 @@
+﻿using ComeBien.DataAccess;
+using ComeBien.Services;
+using ComeBien.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.ComeBien.Tests
+{
+    [TestClass]
+    public class SalesProductVMTest
+    {
+        [TestMethod]
+        public async Task InitializeCollection()
+        {
+            SalesProductVM salesProductVM = new SalesProductVM();
+            int totalIngredients = 0;
+            int totalProducts = 0;
+
+            using (var dbContext = new ComeBienContext())
+            {
+                totalIngredients = await dbContext.Ingredients.CountAsync();
+                totalProducts = await dbContext.Products.CountAsync();
+            }
+
+            Assert.AreEqual(totalProducts, salesProductVM.ProductsCollection.Count);
+            Assert.AreEqual(totalIngredients, salesProductVM.IngredCollection.Count);
+        }
+
+        [TestMethod]
+        public void AddProductToShoppingCartWithoutIngredient()
+        {
+            int idProduct = 1;
+            ShoppingCartService.GetInstance().Reset();
+            SalesProductVM salesProductVM = new SalesProductVM();
+            var product = salesProductVM.ProductsCollection.FirstOrDefault(x => x.Id == idProduct);
+
+            salesProductVM.AddProductCommand.Execute(idProduct);
+
+            Assert.AreEqual(product.Price, ShoppingCartService.GetInstance().ShoppingCart.TotalAmount);
+            Assert.AreEqual(1, ShoppingCartService.GetInstance().ShoppingCart.Products.Count);
+            Assert.IsTrue(ShoppingCartService.GetInstance().ShoppingCart.Products.Any(x=>x.ProductId == idProduct));
+        }
+
+        [TestMethod]
+        public void AddProductToShoppingCartWithIngredient()
+        {
+            int idProduct = 1;
+            ShoppingCartService.GetInstance().Reset();
+            SalesProductVM salesProductVM = new SalesProductVM();
+            var product = salesProductVM.ProductsCollection.FirstOrDefault(x => x.Id == idProduct);
+            salesProductVM.IngredCollection.ToList().ForEach(x => x.Quantity = 1);
+            decimal sumIngredient = salesProductVM.IngredCollection.Sum(x => x.Quantity * x.Price);
+
+            salesProductVM.AddProductCommand.Execute(idProduct);
+
+            Assert.AreEqual(product.Price + sumIngredient, ShoppingCartService.GetInstance().ShoppingCart.TotalAmount);
+            Assert.AreEqual(1, ShoppingCartService.GetInstance().ShoppingCart.Products.Count);
+            Assert.IsTrue(ShoppingCartService.GetInstance().ShoppingCart.Products.Any(x => x.ProductId == idProduct));
+            Assert.IsFalse(ShoppingCartService.GetInstance().ShoppingCart.Products.FirstOrDefault().Ingredients.Any(x=>x.Quantity != 1));
+        }
+    }
+}
---
+++ UnitTest.ComeBien/Tests/ShoppingCartVMTest.cs
@@ -0,0 +1,131 @@
+﻿using ComeBien.Models.Core;
+using ComeBien.Services;
+using ComeBien.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.ComeBien.Tests
+{
+    [TestClass]
+    public class ShoppingCartVMTest
+    {
+        [TestMethod]
+        public void InitializeData()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                TotalAmount = 28,
+                Products = new List<ShoppingCartProduct>()
+                {
+                    new ShoppingCartProduct
+                    {
+                        Price = 10,
+                        ProductId = 1,
+                        Name = "prueba"
+                    },
+                    new ShoppingCartProduct
+                    {
+                        Price = 10,
+                        ProductId = 2,
+                        Name = "prueba"
+                    },
+                    new ShoppingCartProduct
+                    {
+                        Price = 8,
+                        ProductId = 3,
+                        Name = "prueba"
+                    }
+                }
+            };
+            ShoppingCartService.GetInstance().ShoppingCart = cart;
+
+            ShoppingCartVM shoppingCartVM = new ShoppingCartVM();
+
+            Assert.AreEqual(cart.TotalAmount, shoppingCartVM.TotalAmount);
+            Assert.AreEqual(cart.Products.Count, shoppingCartVM.ProductsCollection.Count);
+        }
+
+        [TestMethod]
+        public void RemoveAllProduct()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                TotalAmount = 28,
+                Products = new List<ShoppingCartProduct>()
+                {
+                    new ShoppingCartProduct
+                    {
+                        Price = 10,
+                        ProductId = 1,
+                        Name = "prueba"
+                    },
+                    new ShoppingCartProduct
+                    {
+                        Price = 10,
+                        ProductId = 2,
+                        Name = "prueba"
+                    },
+                    new ShoppingCartProduct
+                    {
+                        Price = 8,
+                        ProductId = 3,
+                        Name = "prueba"
+                    }
+                }
+            };
+            ShoppingCartService.GetInstance().ShoppingCart = cart;
+
+            ShoppingCartVM shoppingCartVM = new ShoppingCartVM();
+
+            shoppingCartVM.RemoveProductCommand.Execute(2);
+            shoppingCartVM.RemoveProductCommand.Execute(1);
+            shoppingCartVM.RemoveProductCommand.Execute(0);
+
+            Assert.AreEqual(0, shoppingCartVM.TotalAmount);
+            Assert.AreEqual(0, shoppingCartVM.ProductsCollection.Count);
+        }
+
+        [TestMethod]
+        public void Remove2Product()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                TotalAmount = 28,
+                Products = new List<ShoppingCartProduct>()
+                {
+                    new ShoppingCartProduct
+                    {
+                        Price = 10,
+                        ProductId = 1,
+                        Name = "prueba"
+                    },
+                    new ShoppingCartProduct
+                    {
+                        Price = 10,
+                        ProductId = 2,
+                        Name = "prueba"
+                    },
+                    new ShoppingCartProduct
+                    {
+                        Price = 8,
+                        ProductId = 3,
+                        Name = "prueba"
+                    }
+                }
+            };
+            ShoppingCartService.GetInstance().ShoppingCart = cart;
+
+            ShoppingCartVM shoppingCartVM = new ShoppingCartVM();
+
+            shoppingCartVM.RemoveProductCommand.Execute(0);
+            shoppingCartVM.RemoveProductCommand.Execute(1);
+
+            Assert.AreEqual(8, shoppingCartVM.TotalAmount);
+            Assert.AreEqual(1, shoppingCartVM.ProductsCollection.Count);
+        }
+    }
+}
